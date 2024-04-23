02a1ad568a83da2fc753b444ee11bc116b5a1572
louis <louis.lamonte@moringaschool.com>
2024-04-23 11:48:32 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,22 @@ function Home() {
     </div>
   );
 }
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
-{/* write an <About> component here */}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
